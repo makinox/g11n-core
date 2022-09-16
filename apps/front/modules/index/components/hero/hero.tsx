7ccb439a221b5814fcd3c61fc83063ae4527450d
Jesus Bossa <x@jesusbossa.dev>
2022-09-16 12:01:17 -0500

    chore: add parsed information to the view

--- apps/front/modules/index/components/hero/hero.tsx
+++ apps/front/modules/index/components/hero/hero.tsx
@@ -1,28 +1,59 @@
+import { FluidContainer, ButtonOutline } from '@makinox/makinox-ui';
+import { Fragment, useMemo } from 'react';
+
 import { useSheet } from '../../../../common/contexts/sheetContext';
 
 const Hero = () => {
   const { languageKeys, languageTuples, sheetTitles } = useSheet();
-  console.log({ languageKeys, languageTuples, sheetTitles });
+
+  const totalData = useMemo(() => {
+    const languageValues = Object.values(languageTuples);
+    const totalTranslationsByLang = languageValues[0] ? Object.values(languageValues[0]).length : 0;
+
+    const totalTranslations: Array<string> = [];
+    languageValues.map((languageValue) => {
+      const valueKeys = Object.values(languageValue);
+      valueKeys.map((value) => totalTranslations.push(value));
+    });
+    const totalEmptyTranslation = totalTranslations.filter((value) => value && value.length === 0).length;
+
+    return { totalTranslations: totalTranslations.length, totalTranslationsByLang, totalEmptyTranslation };
+  }, [languageTuples]);
+
   return (
-    <section>
-      <div>
-        <span>metricas</span>
+    <section className={`flex flex-col ${FluidContainer()}`}>
+      <div className="flex justify-around">
         <div>
-          cuantas traducciones hay
-          {languageKeys.map((lang, index) => (
-            <span key={index}>{lang}</span>
-          ))}
+          <div>
+            <span>cuantas traducciones hay:</span>
+            <div>
+              {languageKeys[0]?.map((lang, index) => (
+                <Fragment key={index}>
+                  <span>{lang}</span>
+                  <span>-</span>
+                </Fragment>
+              ))}
+            </div>
+          </div>
+          <div>
+            <span>Hojas disponibles</span>
+            <div>
+              {sheetTitles.map((titles, index) => (
+                <span key={index}>{titles}</span>
+              ))}
+            </div>
+          </div>
         </div>
-        <span>si es posible cuantos usos de api hay</span>
+
+        <div className="flex flex-col">
+          <span>Total de traducciones: {totalData.totalTranslations}</span>
+          <span>Total de traducciones por idioma: {totalData.totalTranslationsByLang}</span>
+          <span>Total espacios sin traducir: {totalData.totalEmptyTranslation}</span>
+        </div>
       </div>
 
-      <div>
-        <span>hojas disponibles</span>
-        <span>espacios sin traducir</span>
-      </div>
-
-      <div>
-        <span>agregar mas traducciones</span>
+      <div className="flex justify-center">
+        <button className={ButtonOutline()}>agregar mas traducciones</button>
       </div>
     </section>
   );
