557744c58e766f508023b61a49db2607dc45e7dd
Jesus Bossa <x@jesusbossa.dev>
2022-09-18 18:27:18 -0500

    chore: recove form data

--- apps/front/pages/add.tsx
+++ apps/front/pages/add.tsx
@@ -1,17 +1,45 @@
 import { ButtonOutline, FluidContainer } from '@makinox/makinox-ui';
+import { ChangeEvent, FormEvent, useMemo, useState } from 'react';
 import Link from 'next/link';
 
 import { AddPageStyles } from '../modules/add/styles/add.styles';
 import { useSheet } from '../common/contexts/sheetContext';
 import Navbar from '../common/components/Navbar/Navbar';
 
+const FORM_SHEET_NAME = 'sheetName';
+const FORM_KEY_NAME = 'keyName';
+
 const Add = () => {
   const { languageKeys, sheetTitles } = useSheet();
+  const [selectedSheet, setSelectedSheet] = useState(0);
 
   const classes = {
     container: 'flex items-center flex-col',
     fieldSet: 'flex flex-col',
     label: 'text-center',
+  };
+
+  const currentKeyValue = useMemo(() => {
+    return languageKeys[selectedSheet].map((value) => `keyValue-${value}`);
+  }, [languageKeys, selectedSheet]);
+
+  const handleSheetChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const optionElement = event.target;
+    const { selectedIndex } = optionElement;
+    setSelectedSheet(selectedIndex);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formElement = event.target as HTMLFormElement;
+    const formData = {
+      [FORM_SHEET_NAME]: formElement.elements[FORM_SHEET_NAME].value,
+      [FORM_KEY_NAME]: formElement.elements[FORM_KEY_NAME].value,
+    };
+    currentKeyValue.forEach((formKeyValue) =>
+      Object.defineProperty(formData, formKeyValue, { enumerable: true, writable: true, value: formElement.elements[formKeyValue].value })
+    );
+    console.log({ formData });
   };
 
   return (
@@ -26,29 +54,33 @@ const Add = () => {
         <div className={classes.container}>
           <h2>Agregar nuevo elemento</h2>
 
-          <form className={`${AddPageStyles()} ${classes.fieldSet}`}>
+          <form className={`${AddPageStyles()} ${classes.fieldSet}`} onSubmit={handleSubmit}>
             <fieldset className={classes.fieldSet}>
               <label className={classes.label}>Seleccionar hoja</label>
-              <select>
+              <select onChange={handleSheetChange} name={FORM_SHEET_NAME}>
                 {sheetTitles.map((title) => (
-                  <option key={title}>{title}</option>
+                  <option key={title} value={title}>
+                    {title}
+                  </option>
                 ))}
               </select>
             </fieldset>
             <fieldset className={classes.fieldSet}>
               <label className={classes.label}>Nombre de llave*</label>
-              <input type="text" />
+              <input type="text" required name={FORM_KEY_NAME} />
             </fieldset>
-            {languageKeys[0]?.map((langKey) => (
+            {languageKeys[selectedSheet]?.map((langKey, index) => (
               <fieldset key={langKey} className={classes.fieldSet}>
                 <label className={classes.label}>Valor {langKey}*</label>
-                <input type="text" />
+                <input type="text" required name={currentKeyValue[index]} />
               </fieldset>
             ))}
+            <fieldset>
+              <button type="submit" className={ButtonOutline()}>
+                Agregar elemento
+              </button>
+            </fieldset>
           </form>
-        </div>
-        <div className="flex justify-center">
-          <button className={ButtonOutline()}>Agregar elemento</button>
         </div>
       </section>
     </>
